dad22fb81b2d4c364e77f737f669cf8b5489ab84
fchainbackup <115945736+fchainbackup@users.noreply.github.com>
2022-10-16 09:16:25 -1100

    Update asgi.py

--- investment_site/asgi.py
+++ investment_site/asgi.py
@@ -7,16 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'investment_site.settings')
-
-application = get_asgi_application()
-
-
-
 
 
 import os
